fed5b0521433a1326b74b73a8355837aba1d541c
Pranil Dilip Veer <veerpranil@gmail.com>
2024-01-09 11:48:59 +0530

    Boilerplate setup

---
+++ server/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const dotenv = require('dotenv')
+dotenv.config();
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) =>{
+    res.status(200).json({
+        server: "Running",
+    })
+})
+
+// app.get("/health", (req, res) => {
+//     const dbStatus = mongoose.connection.readyState === 1 ? "Connected" : "Disconnected";
+
+//     res.status(200).json({
+//         server: "Running",
+//         database: dbStatus,
+//     });
+// });
+
+// app.use((req, res, next) => {
+//     const error = new Error("Not found");
+//     error.status = 404;
+//     next(error);
+// });
+
+// app.use((err, req, res, next) => {
+//     console.error(err.stack);
+//     res
+//         .status(500)
+//         .json({ error: "Something went wrong! Please try again later." });
+// });
+
+module.exports = app;
---
+++ server/server.js
@@ -0,0 +1,6 @@
+const app = require("./index");
+const PORT = 4000;
+
+app.listen(PORT, ()=> {
+    console.log(`server running at localhost:${PORT }`)
+})
